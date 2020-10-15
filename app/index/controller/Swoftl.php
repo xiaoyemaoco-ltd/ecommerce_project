53239fba68c7198381db83cd5617ed19d5b28114
Administrator <liuqianglan@sina.com>
2020-10-15 16:23:02 +0800

    回调token接受

--- app/index/controller/Swoftl.php
+++ app/index/controller/Swoftl.php
@@ -7,11 +7,15 @@
  */
 namespace app\index\controller;
 use app\BaseController;
+use app\Request;
+use fast\Redis;
 use think\View;
 
 class Swoftl extends BaseController{
 
-    public function index(){
+    public function index(Request $request){
+        $access_token = $request->request('access_token');
+        Redis::set('alibaba_access_token', $access_token, 36000);
         return $this->fetch();
     }
 
